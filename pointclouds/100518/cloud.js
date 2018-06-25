54871b2a112aa396b02042bdd68d35042b3ead4d
MamatsasDhmhtrhs <mamatsas.D@gmail.com>
2018-06-25 17:08:17 +0300

    First commit

---
+++ pointclouds/100518/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=tmerc +lat_0=0 +lon_0=24 +k=0.9996 +x_0=500000 +y_0=0 +ellps=GRS80 +towgs84=-199.87,74.79,246.62,0,0,0,0 +units=m +no_defs",
+    "points": 2542907,
+    "boundingBox": {
+        "lx": 690252.6020050049,
+        "ly": 4323159.667999268,
+        "lz": 29.211000442504884,
+        "ux": 690372.8509979248,
+        "uy": 4323279.9169921879,
+        "uz": 149.45999336242677
+    },
+    "tightBoundingBox": {
+        "lx": 690252.6020049488,
+        "ly": 4323159.667999245,
+        "lz": 29.211000439299999,
+        "ux": 690370.2080077713,
+        "uy": 4323279.91699215,
+        "uz": 48.3110008213
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.0413868427276612,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
---
+++ pointclouds/B1B7-01/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=tmerc +lat_0=0 +lon_0=24 +k=0.9996 +x_0=500000 +y_0=0 +ellps=GRS80 +towgs84=-199.87,74.79,246.62,0,0,0,0 +units=m +no_defs",
+    "points": 14640263,
+    "boundingBox": {
+        "lx": 690292.5963616371,
+        "ly": 4323261.732765198,
+        "lz": 31.25992202758789,
+        "ux": 690322.2680778503,
+        "uy": 4323291.404481411,
+        "uz": 60.93163824081421
+    },
+    "tightBoundingBox": {
+        "lx": 690292.5963616225,
+        "ly": 4323261.732765196,
+        "lz": 31.259922021233,
+        "ux": 690322.2680778529,
+        "uy": 4323278.198822022,
+        "uz": 44.383460998028
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.2569645941257477,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
